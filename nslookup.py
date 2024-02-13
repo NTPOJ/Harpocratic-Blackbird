6d7c5608f77959d33cf5ae83d127c381cb056b0c
Owen Jones <142271569+NTPOJ@users.noreply.github.com>
2024-02-13 20:09:17 +1100

    Added Module Selection to nslookup.py

--- nslookup.py
+++ nslookup.py
@@ -14,3 +14,24 @@ hostname = input("Please enter target website address:\n")
 
 #IP lookup from hostname
 print(f' {hostname} Ip Address is {socket.gethostbyname(hostname)}')
+
+print("[1]. Back to base!")
+print("[2]. More R&D!")
+print("[3]. Cyboogie!")
+print("[4]. Survery their ports!")
+print("[5]. Launch it!")
+
+#Imports scripts for each attack based on number chosen	
+selection = input("What next commrade?")
+if selection == "1":
+	import Harpocratic-Blackbird
+elif selection == "2":
+	import nslookup
+elif selection == "3":
+	import robots_crawler
+elif selection == "4":
+	import BackDoorothy
+ elif selection == "5":
+	import DoS
+else:
+	print("Invalid Selection. Enter 1-5.")
