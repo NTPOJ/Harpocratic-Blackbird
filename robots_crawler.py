e62fec63fbb2978c82630ad7b39973e7042d0ecd
Owen Jones <142271569+NTPOJ@users.noreply.github.com>
2024-02-13 20:09:23 +1100

    Added Module Selection to robots_crawler.py

--- robots_crawler.py
+++ robots_crawler.py
@@ -23,3 +23,24 @@ def get_robots_txt(url):
 	return data.read()
 
 print(get_robots_txt(url))
+
+print("[1]. Back to base!")
+print("[2]. There is always time for Cyboogie!")
+print("[3]. We need R&D!")
+print("[4]. Survery their ports!")
+print("[5]. Launch it!")
+
+#Imports scripts for each attack based on number chosen	
+selection = input("What next commrade?")
+if selection == "1":
+	import Harpocratic-Blackbird
+elif selection == "2":
+	import robots_crawler
+elif selection == "3":
+	import nslookup
+elif selection == "4":
+	import BackDoorothy
+ elif selection == "5":
+	import DoS
+else:
+	print("Invalid Selection. Enter 1-5.")
